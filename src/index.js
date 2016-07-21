2f8df06954b40ab45b1ae8db3e75271f56c70edd
Jochen Rauschenbusch <jochen.rauschenbusch@bridging-it.de>
2016-07-21 22:51:55 +0200

    Format source code

--- src/index.js
+++ src/index.js
@@ -1,36 +1,36 @@
-export default function ({ types: t }) {
- return {
+export default function ({types: t}) {
+  return {
     visitor: {
       CallExpression: function CallExpression(path, state) {
         var opts = state.opts === undefined ? {} : state.opts;
 
         var callee = path.node.callee;
         var args = path.node.arguments;
-      
-		// found an eval potentially containing a require
-        if (t.isIdentifier(callee, { name: 'eval' }) && args.length === 1) {
-            let evaluate = path.get('arguments')[0].evaluate();
 
-            if (!evaluate.confident) {
-              return;
-            }
+        // found an eval potentially containing a require
+        if (t.isIdentifier(callee, {name: 'eval'}) && args.length === 1) {
+          let evaluate = path.get('arguments')[0].evaluate();
 
-            let code = evaluate.value;
-            if (typeof code !== 'string') {
-              return;
-            }
-			
-			code = code.replace(opts.requireName, opts.mappedRequireName);
+          if (!evaluate.confident) {
+            return;
+          }
 
-            path.get('arguments')[0].replaceWith(t.stringLiteral(code));
+          let code = evaluate.value;
+          if (typeof code !== 'string') {
+            return;
+          }
+
+          code = code.replace(opts.requireName, opts.mappedRequireName);
+
+          path.get('arguments')[0].replaceWith(t.stringLiteral(code));
         }
-		
+
         // found a require
-        if (t.isIdentifier(callee, { name: opts.requireName}) && args.length && args.length == 1) {
-          
+        if (t.isIdentifier(callee, {name: opts.requireName}) && args.length && args.length == 1) {
+
           // require('x');
           if (t.isStringLiteral(args[0])) {
-			
+
             var requireModule = args[0].value;
 
             // mirror behaviour at https://github.com/systemjs/systemjs/blob/0.19.8/lib/cjs.js#L50 to remove trailing slash
@@ -39,16 +39,16 @@ export default function ({ types: t }) {
             }
 
             var requireModuleMapped = opts.map && opts.map(requireModule) || requireModule;
-			
+
             path.replaceWith(
               t.callExpression(
                 t.identifier(opts.mappedRequireName),
                 [t.stringLiteral(requireModuleMapped)]
               )
             );
-          } 
-		  // require(expr)';
-		  else {
+          }
+          // require(expr)';
+          else {
             path.replaceWith(
               t.callExpression(
                 t.identifier(opts.mappedRequireName),
@@ -61,9 +61,9 @@ export default function ({ types: t }) {
       Identifier: function Identifier(path, state) {
         var opts = state.opts === undefined ? {} : state.opts;
 
-         if(path.node.name === opts.requireName) {
-           path.node.name = opts.mappedRequireName;
-         }
+        if (path.node.name === opts.requireName) {
+          path.node.name = opts.mappedRequireName;
+        }
       }
     }
   };
